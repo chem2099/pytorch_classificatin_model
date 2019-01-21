46e6209d71d6bcddeb4a19ae8b9f7fe1de0d7cd6
chem2099 <723516632@qq.com>
2019-01-21 09:57:59 +0800

    model测试

--- models/carfar/resnext.py
+++ models/carfar/resnext.py
@@ -11,17 +11,7 @@ class ResNeXtBottleneck(nn.Module):
         resNeXt 有三中类型 type a：inception+残差 使用sum加和
         type b：inception concat 后最好使用1x1的卷积核在卷下 残差
         type c:群组卷积  后最好使用1x1的卷积核在卷下
-        Args:
-            in_channels: input channel dimensionality
-            out_channels: output channel dimensionality
-            stride: conv stride. Replaces pooling layer.
-            cardinality: num of convolution groups.
-            widen_factor: factor to reduce the input dimensionality before convolution.
-        """
-
-class ResNeXtBottleneck(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, cardinality, widen_factor):
-        """ Constructor
+        这边使用了 type c
         Args:
             in_channels: input channel dimensionality
             out_channels: output channel dimensionality
@@ -30,10 +20,13 @@ class ResNeXtBottleneck(nn.Module):
             widen_factor: factor to reduce the input dimensionality before convolution.
         """
         super(ResNeXtBottleneck, self).__init__()
+        # cardinality 分组数 16 输出 256 widen_factor =4
         D = cardinality * out_channels // widen_factor
         self.conv_reduce = nn.Conv2d(in_channels, D, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn_reduce = nn.BatchNorm2d(D)
-        self.conv_conv = nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.conv_conv = nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=cardinality,
+                                   bias=False)
+
         self.bn = nn.BatchNorm2d(D)
         self.conv_expand = nn.Conv2d(D, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn_expand = nn.BatchNorm2d(out_channels)
